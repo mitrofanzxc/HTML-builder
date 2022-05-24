0e9811e2523323e8119f3a953c4b2366ee9b3dff
mitrofanzxc <mitrofanzxc@gmail.com>
2022-05-24 23:23:54 +0300

    init: 04-copy-directory (Tue, May 24, 2022 11:23:53 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,8 @@
+const { mkdir } = require('node:fs');
+const path = require('node:path');
 
+const pathName = path.join(__dirname, 'files-copy');
+
+mkdir(pathName, { recursive: true }, (err) => {
+  if (err) throw err;
+});
