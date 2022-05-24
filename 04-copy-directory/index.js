08e55f3de7c3ba836826e7fa5da30d0b7c901e20
mitrofanzxc <mitrofanzxc@gmail.com>
2022-05-25 00:19:12 +0300

    feat: add solution 04-copy-directory (Wed, May 25, 2022 12:19:11 AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,24 @@
-const { mkdir } = require('node:fs');
+const { mkdir, readdir, createReadStream, createWriteStream } = require('node:fs');
 const path = require('node:path');
 
-const pathName = path.join(__dirname, 'files-copy');
+const pathNameOriginFolder = path.join(__dirname, 'files');
+const pathNameCopyFolder = path.join(__dirname, 'files-copy');
+const options = { withFileTypes: true };
 
-mkdir(pathName, { recursive: true }, (err) => {
+readdir(pathNameOriginFolder, options, (err, files) => {
   if (err) throw err;
+
+  mkdir(pathNameCopyFolder, { recursive: true }, (err) => {
+    if (err) throw err;
+
+    files.forEach((file) => {
+      const pathFileNameOrigin = path.join(pathNameOriginFolder, file.name);
+      const pathFileNameCopy = path.join(pathNameCopyFolder, file.name);
+
+      const readStream = createReadStream(pathFileNameOrigin);
+      const writeStream = createWriteStream(pathFileNameCopy);
+
+      readStream.pipe(writeStream);
+    });
+  });
 });
