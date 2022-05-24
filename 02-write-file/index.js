a06b7668468016d794e9f689f0d0754176ac4c83
mitrofanzxc <mitrofanzxc@gmail.com>
2022-05-24 12:21:22 +0300

    feat: add solution 02-write-file (`data`)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,9 +1,20 @@
-const readline = require('node:readline');
+const path = require('node:path');
+const { open, createWriteStream } = require('node:fs');
 const { stdin: input, stdout: output } = require('node:process');
-
+const readline = require('node:readline');
 const rl = readline.createInterface({ input, output });
 
+const pathName = path.join(__dirname, 'text.txt');
+
+open(pathName, 'a+', (err) => {
+  if (err) throw err;
+});
+
+const textFile = createWriteStream(pathName);
+
 rl.write('Hello, nice to meet you! Please, write something...\n');
+
+input.on('data', (chunk) => textFile.write(chunk));
 
 rl.on('SIGINT', () => {
   console.log('We will miss you, come back soon!');
@@ -11,7 +22,8 @@ rl.on('SIGINT', () => {
 });
 
 rl.on('line', (input) => {
-  if (input === 'exit') {
+  if (input.trim() === 'exit') {
+    console.log('We will miss you, come back soon!');
     rl.close();
   }
 });
